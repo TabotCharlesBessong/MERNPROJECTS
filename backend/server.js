f3bfbe791b12791aff364ed9615b51f734a5f227
Tabot Chares Bessong <ebezebeatrice@gmail.com>
2021-11-24 16:48:46 +0100

    started with backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import {data} from './data.js'
+
+const app = express();
+
+app.get('/api/products',(req,res)=>{
+  res.send(data.products)
+})
+
+app.get('/',(req,res)=>{
+  res.send('Server is up and ready')
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+  console.log(`Server is running at http://localhost:${port}`);
+})
