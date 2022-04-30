6cbdc7c6b38b9c5ddbc5cb8684a79086e7b08dea
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-30 02:37:06 +0100

    connected database

--- JSMEDIAMASTERY/jsbackend/index.js
+++ JSMEDIAMASTERY/jsbackend/index.js
@@ -2,4 +2,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
-import cors from cors
+import cors from 'cors'
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(cors())
+
+const MONGO_URI = 'mongodb+srv://Task1:Project11@node-task-manager.echz8.mongodb.net/TASKMANAGER?retryWrites=true&w=majority'
+
+mongoose.connect(MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=> 
+  app.listen(port,()=> console.log(`The server is running on port ${port}... `) )
+ ).catch((err)=>{
+  console.log(err.message)
+})
