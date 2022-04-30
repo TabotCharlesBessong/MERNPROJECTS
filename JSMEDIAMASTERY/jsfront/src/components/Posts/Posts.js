93f31e64b801c650a262b3d8b1f6cf9fc3e0aa8d
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-30 04:39:23 +0100

    fixing an issue in the frontend and started the redux store

--- JSMEDIAMASTERY/jsfront/src/components/Posts/Posts.js
+++ JSMEDIAMASTERY/jsfront/src/components/Posts/Posts.js
@@ -2,9 +2,12 @@
 import React from 'react'
 import Post from './Post/Post.js'
 import useStyles from './Styles.js'
+import {useSelector} from 'react-redux'
 
 const Posts = () => {
+  const posts = useSelector((state)=> state.posts)
   const classes = useStyles()
+  console.log(posts);
   return (
     <>
       <Post/>
