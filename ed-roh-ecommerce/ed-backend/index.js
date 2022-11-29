15d011a658db3fe7deb30a4beaaecc616fd6fca3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-11-29 15:13:21 +0100

    added bcrypt

--- ed-roh-ecommerce/ed-backend/index.js
+++ ed-roh-ecommerce/ed-backend/index.js
@@ -25,6 +25,7 @@ import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 
 // configuration 
+console.log('Hello world')
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
