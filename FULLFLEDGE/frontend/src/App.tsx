e14e4df7a4070c2c04fe19de5472a29fbeb02dbb
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-28 00:50:13 +0100

    updated home page

--- FULLFLEDGE/frontend/src/App.tsx
+++ FULLFLEDGE/frontend/src/App.tsx
@@ -2,13 +2,20 @@ import React from "react";
 
 import { Refine, AuthProvider } from "@pankod/refine-core";
 import {
-  notificationProvider,
-  RefineSnackbarProvider,
-  CssBaseline,
-  GlobalStyles,
-  ReadyPage,
-  ErrorComponent,
+	notificationProvider,
+	RefineSnackbarProvider,
+	CssBaseline,
+	GlobalStyles,
+	ReadyPage,
+	ErrorComponent,
 } from "@pankod/refine-mui";
+import {
+	AccountCircleOutlined,
+	ChatBubbleOutline,
+	PeopleAltOutlined,
+	StarOutlineRounded,
+	VillaOutlined,
+} from "@mui/icons-material";
 
 import dataProvider from "@pankod/refine-simple-rest";
 import { MuiInferencer } from "@pankod/refine-inferencer/mui";
@@ -17,116 +24,147 @@ import axios, { AxiosRequestConfig } from "axios";
 import { useTranslation } from "react-i18next";
 import { ColorModeContextProvider } from "contexts";
 import { Title, Sider, Layout, Header } from "components/layout";
-import { Login } from "pages/login";
+import {
+	Login,
+	Home,
+	Edit,
+	AllProperties,
+	Agents,
+	MyProfile,
+	AgentProfilePage,
+	PropertyDetails,
+	CreateProperty,
+} from "pages";
 import { CredentialResponse } from "interfaces/google";
 import { parseJwt } from "utils/parse-jwt";
 
 const axiosInstance = axios.create();
 axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
-  const token = localStorage.getItem("token");
-  if (request.headers) {
-    request.headers["Authorization"] = `Bearer ${token}`;
-  } else {
-    request.headers = {
-      Authorization: `Bearer ${token}`,
-    };
-  }
+	const token = localStorage.getItem("token");
+	if (request.headers) {
+		request.headers["Authorization"] = `Bearer ${token}`;
+	} else {
+		request.headers = {
+			Authorization: `Bearer ${token}`,
+		};
+	}
 
-  return request;
+	return request;
 });
 
 function App() {
-  const { t, i18n } = useTranslation();
+	const { t, i18n } = useTranslation();
 
-  const authProvider: AuthProvider = {
-    login: ({ credential }: CredentialResponse) => {
-      const profileObj = credential ? parseJwt(credential) : null;
+	const authProvider: AuthProvider = {
+		login: ({ credential }: CredentialResponse) => {
+			const profileObj = credential ? parseJwt(credential) : null;
 
-      if (profileObj) {
-        localStorage.setItem(
-          "user",
-          JSON.stringify({
-            ...profileObj,
-            avatar: profileObj.picture,
-          })
-        );
-      }
+			if (profileObj) {
+				localStorage.setItem(
+					"user",
+					JSON.stringify({
+						...profileObj,
+						avatar: profileObj.picture,
+					})
+				);
+			}
 
-      localStorage.setItem("token", `${credential}`);
+			localStorage.setItem("token", `${credential}`);
 
-      return Promise.resolve();
-    },
-    logout: () => {
-      const token = localStorage.getItem("token");
+			return Promise.resolve();
+		},
+		logout: () => {
+			const token = localStorage.getItem("token");
 
-      if (token && typeof window !== "undefined") {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
-        axios.defaults.headers.common = {};
-        window.google?.accounts.id.revoke(token, () => {
-          return Promise.resolve();
-        });
-      }
+			if (token && typeof window !== "undefined") {
+				localStorage.removeItem("token");
+				localStorage.removeItem("user");
+				axios.defaults.headers.common = {};
+				window.google?.accounts.id.revoke(token, () => {
+					return Promise.resolve();
+				});
+			}
 
-      return Promise.resolve();
-    },
-    checkError: () => Promise.resolve(),
-    checkAuth: async () => {
-      const token = localStorage.getItem("token");
+			return Promise.resolve();
+		},
+		checkError: () => Promise.resolve(),
+		checkAuth: async () => {
+			const token = localStorage.getItem("token");
 
-      if (token) {
-        return Promise.resolve();
-      }
-      return Promise.reject();
-    },
+			if (token) {
+				return Promise.resolve();
+			}
+			return Promise.reject();
+		},
 
-    getPermissions: () => Promise.resolve(),
-    getUserIdentity: async () => {
-      const user = localStorage.getItem("user");
-      if (user) {
-        return Promise.resolve(JSON.parse(user));
-      }
-    },
-  };
+		getPermissions: () => Promise.resolve(),
+		getUserIdentity: async () => {
+			const user = localStorage.getItem("user");
+			if (user) {
+				return Promise.resolve(JSON.parse(user));
+			}
+		},
+	};
 
-  const i18nProvider = {
-    translate: (key: string, params: object) => t(key, params),
-    changeLocale: (lang: string) => i18n.changeLanguage(lang),
-    getLocale: () => i18n.language,
-  };
+	const i18nProvider = {
+		translate: (key: string, params: object) => t(key, params),
+		changeLocale: (lang: string) => i18n.changeLanguage(lang),
+		getLocale: () => i18n.language,
+	};
 
-  return (
-    <ColorModeContextProvider>
-      <CssBaseline />
-      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-      <RefineSnackbarProvider>
-        <Refine
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-          notificationProvider={notificationProvider}
-          ReadyPage={ReadyPage}
-          catchAll={<ErrorComponent />}
-          resources={[
-            {
-              name: "posts",
-              list: MuiInferencer,
-              edit: MuiInferencer,
-              show: MuiInferencer,
-              create: MuiInferencer,
-              canDelete: true,
-            },
-          ]}
-          Title={Title}
-          Sider={Sider}
-          Layout={Layout}
-          Header={Header}
-          routerProvider={routerProvider}
-          authProvider={authProvider}
-          LoginPage={Login}
-          i18nProvider={i18nProvider}
-        />
-      </RefineSnackbarProvider>
-    </ColorModeContextProvider>
-  );
+	return (
+		<ColorModeContextProvider>
+			<CssBaseline />
+			<GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+			<RefineSnackbarProvider>
+				<Refine
+					dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+					notificationProvider={notificationProvider}
+					ReadyPage={ReadyPage}
+					catchAll={<ErrorComponent />}
+					resources={[
+						{
+							name: "property",
+							list: MuiInferencer,
+							icon: <VillaOutlined />,
+						},
+						{
+							name: "agent",
+							list: MuiInferencer,
+							icon: <PeopleAltOutlined />,
+						},
+						{
+							name: "review",
+							list: MuiInferencer,
+							icon: <StarOutlineRounded />,
+						},
+						{
+							name: "message",
+							list: MuiInferencer,
+							icon: <ChatBubbleOutline />,
+						},
+						{
+							name: "my-profile",
+							options: {
+								label: "My Profile",
+							},
+							list: MuiInferencer,
+							icon: <AccountCircleOutlined />,
+						},
+					]}
+					Title={Title}
+					Sider={Sider}
+					Layout={Layout}
+					Header={Header}
+					routerProvider={routerProvider}
+					authProvider={authProvider}
+					LoginPage={Login}
+					i18nProvider={i18nProvider}
+					DashboardPage={Home}
+				/>
+			</RefineSnackbarProvider>
+		</ColorModeContextProvider>
+	);
 }
 
 export default App;
--- FULLFLEDGE/frontend/src/pages/Home.tsx
+++ FULLFLEDGE/frontend/src/pages/Home.tsx
@@ -1,9 +1,55 @@
 
 import React from 'react'
+import {useList} from '@pankod/refine-core'
+import {PieChart,PropertyReferal,TotalRevenue,TopAgent,PropertyCard} from '../components'
+import {Typography,Box,Stack} from '@pankod/refine-mui'
 
 const Home = () => {
-  return(
-    <div>Hello Home</div>
-  )
+  return (
+		<Box>
+			<Typography fontSize={25} fontWeight={700}>
+				Dashbaord
+			</Typography>
+			<Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+				<PieChart
+					title="Properties for sale"
+					value={684}
+					series={[75, 25]}
+					colors={["#475be8", "#e4ebef"]}
+				/>
+				<PieChart
+					title="Properties for rent"
+					value={674}
+					series={[60, 40]}
+					colors={["#475be8", "#e4ebef"]}
+				/>
+				<PieChart
+					title="Total Customers"
+					value={22378}
+					series={[82, 18]}
+					colors={["#475be8", "#e4ebef"]}
+				/>
+				<PieChart
+					title="Total Cities"
+					value={14}
+					series={[75, 25]}
+					colors={["#475be8", "#e4ebef"]}
+				/>
+			</Box>
+			<Stack
+				mt="25px"
+				width="100%"
+				direction={{
+					xs: "column",
+					lg: "row",
+				}}
+				gap={4}
+        ml={1}
+			>
+				<TotalRevenue />
+				<PropertyReferal />
+			</Stack>
+		</Box>
+	);
 }
 export default Home
