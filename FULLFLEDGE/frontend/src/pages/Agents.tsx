e6b12c6abdbfab4c504b223afccc37e1d786e313
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-03-27 06:58:43 +0100

    updated the agent page

--- FULLFLEDGE/frontend/src/pages/Agents.tsx
+++ FULLFLEDGE/frontend/src/pages/Agents.tsx
@@ -1,9 +1,41 @@
 
-import React from 'react'
+import { useList } from "@pankod/refine-core"
+import { Box, Typography } from "@pankod/refine-mui"
+import { AgentCard } from "components"
 
 const Agents = () => {
+  const {data,isLoading,isError} = useList({
+    resource:'users'
+  })
+  const allAgents = data?.data ?? []
+
+  // if(isLoading) return <Typography>Loading.....</Typography>
+  // if(isError) return <Typography>Something went wrong</Typography>
   return(
-    <div>Hello Agents</div>
+    <Box marginLeft={4} >
+      <Typography fontSize={25} fontWeight={700} color="#11142d">Agents List</Typography>
+
+      <Box
+        mt="20px"
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '20px',
+          backgroundColor: '#fcfcfc'
+        }}
+      >
+        {allAgents.map((agent) =>  (
+          <AgentCard 
+            key={agent._id}
+            id={agent._id}
+            name={agent.name}
+            email={agent.email}
+            avatar={agent.avatar}
+            noOfProperties={agent.allProperties.length}
+          />
+        ))}
+      </Box>
+    </Box>
   )
 }
 export default Agents
