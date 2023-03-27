05cb687eae0e5111f5e4ad5680ffe60e509116d1
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-03-27 16:12:29 +0100

    added edit feature

--- FULLFLEDGE/frontend/src/pages/Edit.tsx
+++ FULLFLEDGE/frontend/src/pages/Edit.tsx
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGetIdentity } from "@pankod/refine-core";
+import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
+import Form from "components";
+import { readFile } from "fs";
+import { resolve } from "dns";
+import { rejects } from "assert";
 
 const Edit = () => {
-	return <div>Hello Edit</div>;
+  const { data: user } = useGetIdentity();
+  const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
+
+	const handleImageChange = (file:File) => {
+		const reader = (readFile:File) => new Promise<string>((resolve,reject) => {
+			const fileReader = new FileReader()
+			fileReader.onload = () => resolve(fileReader.result as string)
+			fileReader.readAsDataURL(readFile)
+		})
+		reader(file).then((result:string) => setPropertyImage({name:file?.name,url:result}))
+	}
+
+	const onFinishHandler = async (data: FieldValues) => {
+    if (!propertyImage.name) return alert('Please upload a property image');
+
+    await onFinish({ ...data, photo: propertyImage.url, email: user.email });
+  };
+
+  return (
+    <Form
+      type="Edit"
+      register={register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      propertyImage={propertyImage}
+    />
+  );
 };
 export default Edit;
