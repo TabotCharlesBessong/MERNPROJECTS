75916b515f8a2c4ee81c2edd11a433c899e44053
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-01-04 06:16:47 +0100

    server updated

--- foodOrderingApp/server/server.js
+++ foodOrderingApp/server/server.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import foodRouter from "./routers/food.routerr"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.use(cors({credentials:true,origin:['http://localhost:5173']}))
+
+app.use('/api/foods',foodRouter)
+
+app.listen(port,() => {
+  console.log(`Port is running on port number ${port}...`)
+})
