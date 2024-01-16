82dde1aaf291b8df1bc828ec3df5eabf407350c1
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-01-16 17:39:16 +0100

    fix: resolved the problem with the connection at the level of the server

--- foodOrderingApp/server/src/constant/images.js
+++ foodOrderingApp/server/src/constant/images.js
@@ -1,16 +1,16 @@
 
 const food1 =
-  "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=600";
+  "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=600";
 const food2 =
-  "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=600";
+  "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=600";
 const food3 =
-  "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=600";
+  "https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=600";
 const food4 =
-  "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=600";
+  "https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=600";
 const food5 =
-  "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=600";
+  "https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=600";
 const food6 =
-  "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=600";
+  "https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=600";
 const halfStar =
   "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=600";
 const emptyStar =
--- foodOrderingApp/server/src/server.js
+++ foodOrderingApp/server/src/server.js
@@ -1,18 +1,16 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import foodRouter from "./router/food.router.js"
-import path from "path"
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import foodRouter from "./router/food.router.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.use(cors({credentials:true,origin:['http://localhost:5173']}))
-// app.use(express.static(path.join(__dirname,'public')))
-app.use('/api/foods',foodRouter)
+app.use(cors({ credentials: true, origin: ["http://localhost:5173"] }));
+app.use("/api/foods", foodRouter);
 
-app.listen(port,() => {
-  console.log(`Port is running on port number ${port}...`)
-})
+app.listen(port, () => {
+  console.log(`Port is running on port number ${port}...`);
+});
