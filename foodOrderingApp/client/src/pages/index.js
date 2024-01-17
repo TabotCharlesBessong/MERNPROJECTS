b03cc78e8cc3216513d3ccdf5a1aa73f8703d694
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-01-17 07:03:57 +0100

    feat:created the login page

--- foodOrderingApp/client/src/pages/index.js
+++ foodOrderingApp/client/src/pages/index.js
@@ -1,3 +1,4 @@
 export {default as HomePage} from './Home/HomePage'
 export {default as FoodPage} from './Food/FoodPage'
 export {default as CartPage} from './Cart/CartPage'
+export {default as LoginPage} from './Login/LoginPage'
