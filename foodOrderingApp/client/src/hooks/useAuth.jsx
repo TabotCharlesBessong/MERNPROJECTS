60fa16784cb73a33c49837646718d9e41deb6347
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-01-17 06:34:19 +0100

    hooks logic

---
+++ foodOrderingApp/client/src/hooks/useAuth.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+import * as userService from "../services/userServices";
+import { toast } from "react-toastify";
+import { useContext } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(userService.getUser());
+
+  const login = async (email, password) => {
+    try {
+      const user = await userService.login(email, password);
+      setUser(user);
+      toast.success("Login Successful");
+    } catch (error) {
+      toast.error(error.response.data);
+    }
+  };
+
+  const logout = () => {
+    userService.logout();
+    setUser(null);
+    toast.success("Logout Successful");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext)
