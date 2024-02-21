97ae942e700a3e32ce9df1ab8602d801e722178e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-02-21 20:19:08 +0100

    starting a brand new project

---
+++ charles_food/server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import cors from "cors"
+
+dotenv.config()
+
+const URI = process.env.MONGO_URI as string
+
+const app = express();
+const port = process.env.PORT as string || 5000
+
+// register a middleware
+app.use(cors({
+  credentials:true,
+  origin:["http://localhost:5137"]
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(express.static("src/public"))
+
+
+mongoose.connect(URI).then(() => {
+  console.log('database is connected')
+}).catch((err) => {
+  console.log(err)
+})
+
+app.listen(port, () => {
+  console.log(`The server is running on port number ${port}....`);
+});
