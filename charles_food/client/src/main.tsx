73ba179c0e721ef210ae1fd550d32e75b33fad58
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-01 05:36:18 +0100

    setting up autho provider

--- charles_food/client/src/main.tsx
+++ charles_food/client/src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from './AppRoutes';
-import "./global.css"
+import AppRoutes from "./AppRoutes";
+import "./global.css";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
