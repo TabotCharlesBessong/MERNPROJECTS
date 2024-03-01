4fd9e9f0952ac4142a947e55284046ea7eb9d816
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-01 03:58:24 +0100

    implemented the header

---
+++ charles_food/client/src/components/header/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "..";
+
+const MainNav = () => {
+  return (
+    <Button
+      className="font-bold hover:text-orange-500 hover:bg-white"
+      variant="ghost"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;
--- charles_food/client/src/components/index.ts
+++ charles_food/client/src/components/index.ts
@@ -1,5 +1,25 @@
 import { Button } from "./ui/button";
+import { Alert } from "./ui/alert";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetTitle,
+  SheetDescription,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
 
-export {default as Header} from "./Header"
+export { default as Header } from "./header/Header";
+export { default as MobileNav } from "./header/MobileNav";
+export { default as MainNav } from "./header/MainNav";
 
-export default {Button}
+export {
+  Button,
+  Alert,
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetTitle,
+  Separator,
+  SheetDescription,
+};
