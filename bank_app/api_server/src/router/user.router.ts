b5605ce6d6298bdf31af12040d5b174639194291
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-21 17:24:03 +0100

    updating user record

--- bank_app/api_server/src/router/user.router.ts
+++ bank_app/api_server/src/router/user.router.ts
@@ -1,8 +1,28 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import UserService from "../services/user.services";
+import UserController from "../controllers/user.controller";
 const createUserRoute = () => {
-  const router = express.Router()
-  router.post("/register")
-  return router
-}
+  const router = express.Router();
+  const userService = new UserService();
+  const userController = new UserController(userService);
 
-export default createUserRoute()
+  router.post("/register", (req: Request, res: Response) => {
+    return userController.register(req, res);
+  });
+
+  router.post("/login", (req: Request, res: Response) => {
+    return userController.login(req, res);
+  });
+
+  router.post("/forgot-password", (req: Request, res: Response) => {
+    return userController.forgotPassword(req, res);
+  });
+
+  router.post("/reset-password", (req: Request, res: Response) => {
+    return userController.resetPassword(req, res);
+  });
+  
+  return router;
+};
+
+export default createUserRoute();
