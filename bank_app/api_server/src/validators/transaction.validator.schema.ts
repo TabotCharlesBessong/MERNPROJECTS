609eda4fb11172433472fc355dae907cb6c6373b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-02 15:26:10 +0100

    created data source, schema and enum for transactions

---
+++ bank_app/api_server/src/validators/transaction.validator.schema.ts
@@ -0,0 +1,12 @@
+import * as yup from "yup"
+
+const initiatePaystackDeposit = yup.object({
+  amount:yup.number().required(),
+  accountId:yup.string().trim().required()
+})
+
+const ValidationSchema = {
+  initiatePaystackDeposit
+}
+
+export default ValidationSchema
