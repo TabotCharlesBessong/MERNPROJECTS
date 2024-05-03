dff5d88dd2721e5c3a53b6ddd76c107e3b3da481
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-03 17:08:55 +0100

    created payment services and updated transaction controller

--- bank_app/api_server/src/controllers/transaction.controller.ts
+++ bank_app/api_server/src/controllers/transaction.controller.ts
@@ -1,19 +1,55 @@
 import { Request, Response } from "express";
 import TransactionService from "../services/transaction.service";
+import PaymentService from "../services/payment.services";
+import Utility from "../utils/index.utils";
+import { ResponseCode } from "../interfaces/enum/code.enum";
 
 class TransactionController {
-  private transactionService:TransactionService
+  private transactionService: TransactionService;
 
-  constructor(_transactionService:TransactionService){
-    this.transactionService = _transactionService
+  constructor(_transactionService: TransactionService) {
+    this.transactionService = _transactionService;
   }
 
-  async initiatePaystackDeposit(req:Request,res:Response){
+  async initiatePaystackDeposit(req: Request, res: Response) {
     try {
-      const params = {...req.body}
-      // const depositInfo = await 
+      const params = { ...req.body };
+      const depositInfo = await PaymentService.generatePaystackPaymentUrl(
+        params.user.email,
+        params.amount
+      );
+      if (!depositInfo) {
+        return Utility.handleError(
+          res,
+          "Paystack payment not available, try again in a few seconds please",
+          ResponseCode.NOT_FOUND
+        );
+      }
+      const newTransaction = {
+        userId: params.user.id,
+        accountId: params.accountId,
+        amount: params.amount,
+        reference: depositInfo.reference,
+        detail: {},
+      };
+
+      let deposit = await this.transactionService.depositByPayStack(
+        newTransaction
+      );
+      return Utility.handleSuccess(
+        res,
+        "Transaction created successfully",
+        { transaction: deposit, url: depositInfo.authorization_url },
+        ResponseCode.SUCCESS
+      );
     } catch (error) {
-      
+      return Utility.handleError(
+        res,
+        (error as TypeError).message,
+        ResponseCode.SERVER_ERROR
+      );
     }
   }
 }
+
+export default TransactionController
