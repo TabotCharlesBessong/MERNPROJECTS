c68e48a40695b0cf59f15a534306eeb339cffccc
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-25 13:54:56 +0100

    getting user auth status

--- job_fidr/client/app/layout.tsx
+++ job_fidr/client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ContextProvider from "@/providers/ContextProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ContextProvider>{children}</ContextProvider>
       </body>
     </html>
   );
--- job_fidr/client/app/page.tsx
+++ job_fidr/client/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="">
-      <h1>Hello world</h1>
+      <Link href={"http://localhost:5000/login"} >
+      Login</Link>
     </div>
   );
 }
