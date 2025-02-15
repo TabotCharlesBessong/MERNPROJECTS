6e1b21be46ea1a0da8a49b0c896c960cea391560
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-02-15 13:39:17 +0100

    completing with the admin endpoint

--- metaverse_game/metaverse/apps/http/src/routes/index.ts
+++ metaverse_game/metaverse/apps/http/src/routes/index.ts
@@ -1,15 +1,16 @@
-import client from '@repo/db/client';
+import client from "@repo/db/client";
 import { Request, Response, Router } from "express";
 import { SigninSchema, SignupSchema } from "../types";
-import { compare, hash } from '../scrypt';
-import { JWT_PASSWORD } from '../config';
+import { compare, hash } from "../scrypt";
+import { JWT_PASSWORD } from "../config";
 import jwt from "jsonwebtoken";
-import { userRouter } from './user';
-import { spaceRouter } from './space';
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
 
-export const router = Router()
+export const router = Router();
 
-router.post("/signup", async (req:Request, res:Response) => {
+router.post("/signup", async (req: Request, res: Response) => {
   console.log("Inside signup, received body:", req.body);
 
   // Parse request data
@@ -21,7 +22,7 @@ router.post("/signup", async (req:Request, res:Response) => {
       message: "Validation failed",
       errors: parsedData.error.format(),
     });
-    return
+    return;
   }
 
   const hashedPassword = await hash(parsedData.data.password);
@@ -40,7 +41,6 @@ router.post("/signup", async (req:Request, res:Response) => {
     res.status(400).json({ message: "User already exists" });
   }
 });
-
 
 router.post("/signin", async (req, res) => {
   const parsedData = SigninSchema.safeParse(req.body);
@@ -83,5 +83,31 @@ router.post("/signin", async (req, res) => {
   }
 });
 
+router.get("/elements", async (req, res) => {
+  const elements = await client.element.findMany();
+
+  res.json({
+    elements: elements.map((e) => ({
+      id: e.id,
+      imageUrl: e.imageUrl,
+      width: e.width,
+      height: e.height,
+      static: e.static,
+    })),
+  });
+});
+
+router.get("/avatars", async (req, res) => {
+  const avatars = await client.avatar.findMany();
+  res.json({
+    avatars: avatars.map((x) => ({
+      id: x.id,
+      imageUrl: x.imageUrl,
+      name: x.name,
+    })),
+  });
+});
+
 router.use("/user", userRouter);
 router.use("/space", spaceRouter);
+router.use("/admin", adminRouter);
