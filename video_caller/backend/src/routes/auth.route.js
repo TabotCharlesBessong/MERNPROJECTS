73dad0de88bd450785453c28097e062c20924958
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-12 15:22:13 +0100

    signup, login and logout

---
+++ video_caller/backend/src/routes/auth.route.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  login,
+  logout,
+  // onboard,
+  signup,
+} from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+// router.post("/onboarding", protectRoute, onboard);
+
+// check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
+
+export default router;
--- video_caller/backend/src/server.js
+++ video_caller/backend/src/server.js
@@ -2,18 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
